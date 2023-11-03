ae2e328efdec5ad1b8b33b1a1f045d9d7195c7b4
TheNiska <denis.bond.x@gmail.com>
2023-11-03 15:56:23 +0300

    models and migrations

--- first_project/first_app/models.py
+++ first_project/first_app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class MenuCategory(models.Model):
+    category_name = models.CharField(max_length=200)
+
+class MenuItem(models.Model):
+    menu_item = models.CharField(max_length=200)
+    price = models.IntegerField(null=False)
+    category_id = models.ForeignKey(MenuCategory, on_delete=models.PROTECT,
+                                    default=None, related_name='category')
+
